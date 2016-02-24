b530940c0209050155550700bcd4c1d1e03597f6
jhenderson88 <henderson2534@gmail.com>
2016-02-24 11:35:56 -0800

    Update RunYields.C

--- RunYields.C
+++ RunYields.C
@@ -1,8 +1,8 @@
 {
 
-  gSystem->Load("/home/jhenderson/YieldGrabber/YieldGrabber_cxx.so");
+  gSystem->Load("/home/jhenderson/YieldGrabber/YieldGrabber_cxx.so"); // Edit this address to something appropriate
   YieldGrabber t;
-  t.Start("/home/jhenderson/YieldGrabber/Data/");
+  t.Start("/home/jhenderson/YieldGrabber/Data/"); // Edit this address to something appropriate
   t.PlotMeanIntensity();   
 
 }
